0b10a545027c7db479adcf35764d8c33402ff693
RPG _player#1 <155161497+kartikeybhardwaj08@users.noreply.github.com>
2026-06-11 12:20:43 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Download and install yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
+chmod +x yt-dlp
+# Install npm packages
+npm install
